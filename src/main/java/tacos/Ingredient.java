c98b058b8838f9f58a04161adb7601114d2cbf80
ohjm <meyoub612@naver.com>
2022-02-17 21:12:53 +0900

    스프링 시큐리티

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 
 @Data
 @RequiredArgsConstructor
-@NoArgsConstructor(access= AccessLevel.PRIVATE, force=true)
+@NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
 @Entity
 public class Ingredient {
 
@@ -18,6 +18,7 @@ public class Ingredient {
     private final String id;
     private final String name;
     private final Type type;
+
 
     public static enum Type{
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
